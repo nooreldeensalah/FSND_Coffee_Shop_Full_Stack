dfa2aad1433603af45489afa9a24adffde98fd4a
Noor Eldeen Salah <m-2123325228@s-mu.edu.eg>
2021-07-09 00:18:43 +0200

    Add Auth0 configurations

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'nooreldeensalah2.us', // the auth0 domain prefix
+    audience: 'Coffee', // the audience set for the auth0 app
+    clientId: 'Rn0vXKb5RZtVfxkotW8AVe5Bi3UfyZk2', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
